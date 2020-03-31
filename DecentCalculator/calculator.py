0fb1a36588e17746692968e1c4391bc424f90154
Guillermo Burgos <burgos27@outlook.com>
2020-03-31 19:23:14 -0400

    Adding try-except blocks

--- DecentCalculator/calculator.py
+++ DecentCalculator/calculator.py
@@ -1,8 +1,7 @@
 class Calc:
     """ A class modeling the functions of a calculator """
     
-    def __init__(self):
-        self.user_input = -1
+    #def __init__(self):
 
     def menu(self):
         """ Will output Calculator Menu """
@@ -18,46 +17,77 @@ class Calc:
     def prompt_input(self):
         """ Will allow the user to input a choice based off the menu """
         prompt = "\nWelcome to our basic calculator! Please enter an option: "
-        self.user_input = int(input(prompt))
-        #return user_input
+        user_input = input(prompt)
+        return user_input
         
     def addition(self):
         """ Asks the user for x and y, adds using x + y, prints total """
-        x = int(input("Please enter the first number you wish to add: "))
-        y = int(input("Please enter the second number you wish to add: "))
-                            
-        total = x + y
-        print(f"{x} + {y} = {total}")
-
+        while True:
+            try:
+                x = int(input("Please enter the first number you wish to add:" 
+                    " "))
+                y = int(input("Please enter the second number you wish to" 
+                    "add: "))
+                
+                total = x + y
+                print(f"{x} + {y} = {total}")
+                return None
+                    
+            except ValueError:
+                print("Please make sure to enter numerical values...")
+                
     def subtraction(self):
         """ Asks the user for x and y, subtracts using x - y, prints total """
-        x = int(input("Please enter the first number you wish to "
-            "subtract: "))
-        y = int(input("Please enter the second number you wish to "
-            "subtract: "))
-                                
-        total = x - y
-        print(f"{x} - {y} = {total}")
+        while True:
+            try:
+                x = int(input("Please enter the first number you wish to "
+                    "subtract: "))
+                y = int(input("Please enter the second number you wish to "
+                    "subtract: "))
+                    
+                total = x - y
+                print(f"{x} - {y} = {total}")
+                return None
+                    
+            except ValueError:
+                print("Please make sure to enter numerical values...")
 
     def multiplication(self):
         """ Asks the user for x and y, multiplies using x * y, prints total """
-        x = int(input("Please enter the first number you wish to"
-            " multiply: "))
-        y = int(input("Please enter the second number you wish to"
-            " multiply: "))
-                              
-        total = x * y
-        print(f"{x} * {y} = {total}")
-    
+        while True:
+            try:
+                x = int(input("Please enter the first number you wish to"
+                    " multiply: "))
+                y = int(input("Please enter the second number you wish to"
+                    " multiply: "))
+                    
+                total = x * y
+                print(f"{x} * {y} = {total}")
+                return None
+                    
+            except ValueError:
+                print("Please make sure to enter numerical values...")
+
     def division(self):
         """ Asks the user for x and y, divides using x / y, prints total """
-        x = int(input("Please enter the first number you wish to divide:"
-            " "))
-        y = int(input("Please enter the second number you wish to divide:"
-            " "))
-                                
-        total = x / y
-        print(f"{x} / {y} = {total}")
+        while True:
+            try:
+                x = int(input("Please enter the first number you wish to "
+                    "divide: "))
+                y = int(input("Please enter the second number you wish to "
+                    "divide: "))
+                    
+                total = x / y
+                print(f"{x} / {y} = {total}")
+                return None
+                    
+            except ValueError:
+                print("Please make sure to enter numerical values...")
+              
+            except ZeroDivisionError:
+                print("You can't divide by 0!")
+                
+            
         
     def control(self):
         """ 
@@ -65,25 +95,29 @@ class Calc:
         functions, or to end the program
         """
         quit = False
+        user_input = -1
         
         print(self.menu())
         
         while quit == False:
-            self.prompt_input()
-            if(self.user_input == 0):
-                quit = True
-                break
-        
-            elif(self.user_input == 1):
-                self.addition()
-            elif(self.user_input == 2):
-                self.subtraction()
-            elif(self.user_input == 3):
-                self.multiplication()
-            elif(self.user_input == 4):
-                self.division()
+            try:
+                user_input = int(self.prompt_input())
+                if(user_input == 0):
+                    quit = True
+                    break
             
-            print()
-            print(self.menu())
+                elif(user_input == 1):
+                    self.addition()
+                elif(user_input == 2):
+                    self.subtraction()
+                elif(user_input == 3):
+                    self.multiplication()
+                elif(user_input == 4):
+                    self.division()
+                
+                print()
+                print(self.menu())
+            except ValueError:
+                print("Please choose an option on the menu...")
         
         print("\nThank you for using the basic calculator!!") 
