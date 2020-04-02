e4f213a3d5a0b0ef8fd60d0b293aa808c7a59f1f
Guillermo Burgos <burgos27@outlook.com>
2020-04-02 10:22:05 -0400

    Added funct to options in the menu

--- StudentRecords/control.py
+++ StudentRecords/control.py
@@ -1,5 +1,8 @@
 def unpack(line):
-    """ Unpack the information from the line into seperate variables """
+    """ 
+    Unpack the information from the line into seperate variables 
+    returns a dict called student
+    """
     student = {
             'firstName' : '',
             'lastName' : '',
@@ -32,7 +35,7 @@ def unpack(line):
     return student
     
 def readFromFile(filename):
-    """ Read from filename, return the data """
+    """ Read from filename, return the data as a dict """
     
     listOfStudents = []
     
@@ -65,20 +68,78 @@ def menu():
     while True:
         try:
             user = int(user)
-            print("".center(80, '*'))
-            print("".center(80, '*'))
             return user
         except ValueError:
             pass
         user = input("Invalid entry, please input a value from the menu: ")
     
-    
+def displayFormat(line):
+    """ format the information passed in, return it as a string """
+    return(f"{line['firstName'].ljust(9, ' ')} "
+            f"{line['lastName'].ljust(15, ' ')} {line['id'].ljust(15, ' ')} "
+            f"{line['dept'].ljust(10, ' ')} "
+            "{:.2f}"
+            f"{line['status'].rjust(7, ' ')} "
+            f"{line['dateOfGrad'].rjust(15, ' ')}".format(line['gpa']))
+
 def displayAll(data):
-    """ Print out data in neat format """
+    """ Prints out all students in data in neat format """
+    
+    print("".center(80, '*'))
+    print("".center(80, '*'))
     
     for line in data:
-        print(f"{line['firstName'].ljust(9, ' ')} "
-            f"{line['lastName'].ljust(15, ' ')} {line['id'].ljust(15, ' ')} "
-            f"{line['dept'].ljust(10, ' ')} {line['gpa']} "
-            f"{line['status'].rjust(7, ' ')} "
-            f"{line['dateOfGrad'].rjust(15, ' ')}")
+        print(displayFormat(line))
+        
+            
+def displayGrads(data):
+    """ Print out only students who have graduated """
+    
+    print("".center(80, '*'))
+    print("".center(80, '*'))
+    
+    for line in data:
+        if line['status'] == 'Y':
+            print(displayFormat(line))
+                
+def displayGPA(data):
+    """ Print out students who's GPA is a certain amnt """
+    
+    user_amnt = input("Please input the GPA amount: ")
+    
+    while True:
+        try:
+            user_amnt = float(user_amnt)
+            break
+        except ValueError:
+            pass
+            
+        user_amnt = input("Invalid entry, please enter a number: ")
+    
+    
+    print("".center(80, '*'))
+    print("".center(80, '*'))
+    
+    for line in data:
+        if line['gpa'] >= user_amnt:
+            print(displayFormat(line))
+                
+def displayDept(data):
+    """ Print out students who belong to a certain Major """
+    
+    flag = False
+    user_in = input("Please enter major filter: ")
+    
+    print("".center(80, '*'))
+    print("".center(80, '*'))
+    
+    for line in data:
+        if user_in.lower() == line['dept'].lower():
+            print(displayFormat(line))
+            flag = True
+    
+    if flag == False:
+        print(f"Sorry, we could not find any Major matching {user_in} "
+            "Please make sure input is correct.")
+        
+            
--- StudentRecords/main.py
+++ StudentRecords/main.py
@@ -12,6 +12,12 @@ user = c.menu()
 
 if user == 0:
     c.displayAll(data)
+elif user == 1:
+    c.displayGrads(data)
+elif user == 2:
+    c.displayGPA(data)
+elif user == 3:
+    c.displayDept(data)
 elif user == 4:
     print("\nExiting program now... \n")
 
